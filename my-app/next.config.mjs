85b63aa925b42d5978448dcc0e1570dbf9597152
xian lee <shanleyclarence@gmail.com>
2025-01-17 13:17:55 +0000

    Remove my-app directory

--- my-app/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['news.llu.edu','i0.wp.com'], // Add allowed external domains here
-    },
-  };
-  
-  export default nextConfig;
-  
